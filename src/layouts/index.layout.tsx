c6ed16a4287074c37fbc41e6a9c2f44cc8831466
Krisanapong Jaruenjanporn <150851052+KrisanapongMonco@users.noreply.github.com>
2025-03-20 22:34:25 +0700

    update

--- src/layouts/index.layout.tsx
+++ src/layouts/index.layout.tsx
@@ -80,41 +80,7 @@ export default function DashboardLayoutBasic(props: any) {
     >
       <DashboardLayout>
         <PageContainer>
-          <Grid container spacing={1}>
-            <Grid size={5} />
-            <Grid size={12}>
-              <Skeleton height={14} />
-            </Grid>
-            <Grid size={12}>
-              <Skeleton height={14} />
-            </Grid>
-            <Grid size={4}>
-              <Skeleton height={100} />
-            </Grid>
-            <Grid size={8}>
-              <Skeleton height={100} />
-            </Grid>
-
-            <Grid size={12}>
-              <Skeleton height={150} />
-            </Grid>
-            <Grid size={12}>
-              <Skeleton height={14} />
-            </Grid>
-
-            <Grid size={3}>
-              <Skeleton height={100} />
-            </Grid>
-            <Grid size={3}>
-              <Skeleton height={100} />
-            </Grid>
-            <Grid size={3}>
-              <Skeleton height={100} />
-            </Grid>
-            <Grid size={3}>
-              <Skeleton height={100} />
-            </Grid>
-          </Grid>
+          <Grid container spacing={1}></Grid>
         </PageContainer>
         <Footer />
       </DashboardLayout>
